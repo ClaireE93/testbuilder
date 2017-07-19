71f0454852d1f5db033ed3124c078f38cd2c7107
Claire Johnson <c.eliseee@gmail.com>
2017-07-19 15:11:48 -0700

    Working Diner's Club and AmEx test (part 1).

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -12,7 +12,17 @@ var detectNetwork = function(cardNumber) {
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
+// Visa always has a prefix of 4 and a length of 13, 16, or 19.
+// MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  let length = cardNumber.length;
+  let cardStart = function(){
+    if(cardNumber[0] === '3' && (cardNumber[1] === '8' || cardNumber[1] === '9')) return 'dinersclub';
+    if(cardNumber[0] === '3' && (cardNumber[1] ==='4' || cardNumber[1] ==='7')) return 'amex';
+  }
+
+  if(length === 14 && cardStart() === 'dinersclub') return 'Diner\'s Club';
+  if(length === 15 && cardStart() === 'amex') return 'American Express';
+
 };
-
-
